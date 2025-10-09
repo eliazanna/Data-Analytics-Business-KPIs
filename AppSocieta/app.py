e16831ceebbcf7b1533d83bb2cbc13e4c2560c2b
Elia Zanini <elizan01@libero.it>
2025-10-10 00:44:16 +0200

    app.py
    i want it to look for api credentials online (secrets: streamlit) and not on pc

--- AppSocieta/app.py
+++ AppSocieta/app.py
@@ -1,17 +1,22 @@
+import streamlit as st
 import gspread
 from google.oauth2.service_account import Credentials
 
+#access to google sheets- scopes
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
 ]
 
-creds = Credentials.from_service_account_file(
-    r"D:\DESKTOP\Desktop\AppSocieta\creds.json",
+#using streamlit "secret" - where apis password is hided
+creds = Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],  # <-- legge dal pannello "Secrets"
     scopes=SCOPES
 )
 client = gspread.authorize(creds)
 
 SHEET_NAME = "GestioneSocieta"
+
+#sheets access
 prodotti_ws = client.open(SHEET_NAME).worksheet("prodotti")
 vendite_ws = client.open(SHEET_NAME).worksheet("vendite")
